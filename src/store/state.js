ec02731bc91b8e8c5654c2cb57c7dd0da4ec8582
MIRAN <kmr@kcnet.co.kr>
2021-02-07 23:42:52 +0900

    vuex, axios 자유게시판에 적용 중

---
+++ src/store/state.js
@@ -0,0 +1,19 @@
+const state = {
+  freeBoardList: [
+    {
+      id: '1',
+      title: 'test1',
+      date: '2021-01-01',
+    },
+    {
+      id: '2',
+      title: 'test2',
+      date: '2021-01-02',
+    },
+  ],
+  freeBoard: {
+    lists: [],
+  },
+};
+
+export default state;
