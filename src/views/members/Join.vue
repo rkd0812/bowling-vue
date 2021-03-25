61832438ccf2cb9c4c19abb15034965961857b22
miran <kmr@kcnet.co.kr>
2021-03-25 13:14:59 +0900

    input 검증 로직 추가

--- src/views/members/Join.vue
+++ src/views/members/Join.vue
@@ -6,15 +6,15 @@
         <div class="form-group">
           <label class="control-label col-4"><span class="text-danger">*</span> 이메일</label>
           <div class="col-8 email">
-            <input type="text" class="form-control"> @
+            <input
+              type="text"
+              class="form-control"
+              v-model="email"> @
             <select class="form-control">
               <option>직접입력</option>
               <option>gmail.com</option>
               <option>naver.com</option>
             </select>
-            <span class="text-danger text-small"><i class="fa fa-times"></i>
-              비밀번호는 특수문자, 소문자, 숫자를 이용하여 8자 ~ 20자 이내로 작성하여 주세요.
-            </span><br>
             <span class="text-info text-small">
               <i class="fa fa-check"></i> 사용할 수 있는 이메일 입니다.
             </span>
@@ -23,15 +23,30 @@
         <div class="form-group">
           <label class="control-label col-4"><span class="text-danger">*</span> 비밀번호</label>
           <div class="col-8">
-            <input type="text" class="form-control">
+            <input type="password"
+                   class="form-control"
+                   v-model="password1">
+            <span
+              class="text-danger text-small"
+              v-show="errMsgPw"><i class="fa fa-times"></i>
+              비밀번호는 특수문자, 소문자, 숫자를 이용하여 8자 ~ 20자 이내로 작성하여 주세요.
+            </span>
           </div>
         </div>
         <div class="form-group">
           <label class="control-label col-4"><span class="text-danger">*</span> 비밀번호 확인</label>
           <div class="col-8">
-            <input type="text" class="form-control">
-            <span class="text-danger text-small"><i class="fa fa-times"></i> 비밀번호를 확인하세요</span><br>
-            <span class="text-info text-small">
+            <input type="password"
+                   class="form-control"
+                   v-model="password2"
+                   v-bind:disabled="this.errMsgPw">
+            <span
+              class="text-danger text-small"
+              v-show="errMsgPwConfirm">
+              <i class="fa fa-times"></i> 비밀번호를 확인하세요</span>
+            <span
+              class="text-info text-small"
+              v-show="msgPwConfirm">
               <i class="fa fa-check"></i> 비밀번호가 확인되었습니다.
             </span>
           </div>
@@ -45,9 +60,15 @@
         <div class="form-group">
           <label class="control-label col-4"><span class="text-danger">*</span> 모바일</label>
           <div class="col-8 connect">
-            <input type="text" class="form-control"> -
-            <input type="text" class="form-control"> -
-            <input type="text" class="form-control">
+            <input type="text"
+                   class="form-control"
+                   v-model="phoneNum1"> -
+            <input type="text"
+                   class="form-control"
+                   v-model="phoneNum2"> -
+            <input type="text"
+                   class="form-control"
+                   v-model="phoneNum3">
           </div>
         </div>
       </div>
@@ -78,6 +99,65 @@ export default {
   components: {
     SubTitle,
     ButtonWrap,
+  },
+  data() {
+    return {
+      email: '',
+      password1: '',
+      password2: '',
+      errMsgPw: true,
+      errMsgPwConfirm: false,
+      msgPwConfirm: false,
+      phoneNum1: null,
+      phoneNum2: null,
+      phoneNum3: null,
+    };
+  },
+  watch: {
+    password1(value) {
+      this.checkPassword(value);
+      this.checkPasswordMatch();
+    },
+    password2() {
+      this.checkPasswordMatch();
+    },
+    phoneNum1() {
+      this.phoneNum1 = this.phoneNum1.replace(/[^0-9]/g, '');
+    },
+    phoneNum2() {
+      this.phoneNum2 = this.phoneNum2.replace(/[^0-9]/g, '');
+    },
+    phoneNum3() {
+      this.phoneNum3 = this.phoneNum3.replace(/[^0-9]/g, '');
+    },
+  },
+  methods: {
+    checkPassword(pw) {
+      const num = pw.search(/[0-9]/g);
+      const engLo = pw.search(/[a-z]/g);
+      const engUp = pw.search(/[A-Z]/g);
+      const spe = pw.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+
+      if (pw.length < 8 || pw.length > 20) {
+        this.errMsgPw = true;
+      } else if (pw.search(/\s/) !== -1) {
+        this.errMsgPw = true;
+      } else if (num < 0 || engLo < 0 || spe < 0 || engUp < 0) {
+        this.errMsgPw = true;
+      } else {
+        this.errMsgPw = false;
+      }
+    },
+    checkPasswordMatch() {
+      if (this.errMsgPw) return;
+      if (this.password2 !== this.password1) {
+        this.errMsgPwConfirm = true;
+        this.msgPwConfirm = false;
+      } else {
+        this.errMsgPwConfirm = false;
+        this.msgPwConfirm = true;
+      }
+    },
   },
 };
 </script>
